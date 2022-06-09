8f0e8666baea659e7845e4a38e1bea88cf36f67d
aswanip-git <aswanipkommeri@gmail.com>
2022-06-09 20:56:49 +0530

    Navbar Section - updated class name in camelcase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ import AppLogo from "./images/navbar-logo.png";
 
 function Navbar() {
     return (
-        <nav className="app-header">
+        <nav className="appHeader">
             <div>
-                <img className="app-logo" src={AppLogo} alt="logo"/>
+                <img className="appLogo" src={AppLogo} alt="logo"/>
             </div>
-            <div className='app-menu'>
-                <a href="#" className="app-links">Home</a>
-                <a href="#" className='app-links'>Product</a>
-                <a href="#" className='app-links'>Contact</a>
-                <a href="#" className='app-links'>About</a>
+            <div className='appMenu'>
+                <a href="#" className="appLinks">Home</a>
+                <a href="#" className='appLinks'>Product</a>
+                <a href="#" className='appLinks'>Contact</a>
+                <a href="#" className='appLinks'>About</a>
             </div>
-            <div className="app-login">
-                <a href="#" className="login app-links">Login</a>
+            <div className="appLogin">
+                <a href="#" className="login appLinks">Login</a>
             </div>
             
         </nav>
