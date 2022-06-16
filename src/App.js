e3e65d4e658db44504d2e67293670810ad688846
aswanip-git <aswanipkommeri@gmail.com>
2022-06-16 21:48:28 +0530

    subHeader Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import SubHeader from "./components/SubHeader"
 import './App.css';
+import './subHeader.css';
 
 function App() {
-    return (
-        <div className="App">
-            
+    return(
+        <div className="app">
             <Navbar />
+            <SubHeader />
         </div>
     )
 }
