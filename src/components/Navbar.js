0a62993c944f78789d1ed6593856d58926bd2f4f
aswanip-git <aswanipkommeri@gmail.com>
2022-07-03 20:37:27 +0530

    moved images folder from components to src

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,32 @@
-import React from 'react'; 
-import AppLogo from "./images/navbar-logo.png";
-
+import React from "react";
+import AppLogo from "../images/navbar-logo.png";
 
 function Navbar() {
-    return (
-        <nav className="appHeader">
-            <div>
-                <img className="appLogo" src={AppLogo} alt="logo"/>
-            </div>
-            <div className='appMenu'>
-                <a href="#" className="appLinks">Home</a>
-                <a href="#" className='appLinks'>Product</a>
-                <a href="#" className='appLinks'>Contact</a>
-                <a href="#" className='appLinks'>About</a>
-            </div>
-            <div className="appLogin">
-                <a href="#" className="login appLinks">Login</a>
-            </div>
-            
-        </nav>
-    )
+  return (
+    <nav className="appHeader">
+      <div>
+        <img className="appLogo" src={AppLogo} alt="logo" />
+      </div>
+      <div className="appMenu">
+        <a href="#" className="appLinks">
+          Home
+        </a>
+        <a href="#" className="appLinks">
+          Product
+        </a>
+        <a href="#" className="appLinks">
+          Contact
+        </a>
+        <a href="#" className="appLinks">
+          About
+        </a>
+      </div>
+      <div className="appLogin">
+        <a href="#" className="login appLinks">
+          Login
+        </a>
+      </div>
+    </nav>
+  );
 }
-export default Navbar
+export default Navbar;
