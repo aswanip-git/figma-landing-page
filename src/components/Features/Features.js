1ab9dfe4d3b88ee8b51620195b6adef2f3fcc270
aswanip-git <aswanipkommeri@gmail.com>
2022-07-12 21:21:33 +0530

    features section - updated

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,77 +1,75 @@
 import React from "react";
 import "./Features.css";
-import Frame from "../../images/frame.png";
-import Menu from "../../images/restaurant-menu.png";
-import Infinity from "../../images/infinity.png";
-import Vector from "../../images/vector.png";
+import Frame from "../../images/frame.svg";
+import Interface from "../../images/intuitive-interface.svg";
+import Infinity from "../../images/infinity.svg";
+import Vector from "../../images/vector.svg";
 
 function Features() {
   return (
     <div className="features">
-      <div className="featuresHeader">
-        <div className="featureHead">
-          <p className="head">Features</p>
-          <p className="featureSubHead">
-            <span className="featureText">
-              Most calendars are designed for teams. Slate is designed for
-              <br />
-              freelancers who want a simple way to plan their schedule.
-            </span>
-          </p>
+      <div className="featureHead">
+        <p className="head">Features</p>
+        <p className="featureSubHead">
+          Most calendars are designed for teams. Slate is designed for
+          <br />
+          freelancers who want a simple way to plan their schedule.
+        </p>
+      </div>
+
+      <div className="featuresFrame">
+        <div className="frameImgDiv">
+          <img className="frameImg" src={Frame} alt="frame" />
         </div>
-        <div className="featuresFrame">
-          <div className="frame">
-            <img className="frameImg" src={Frame} alt="frame" />
+
+        <div className="frameContentDiv">
+          <div className="featureCard">
+            <div className="featureCardBody">
+              <div>
+                <img src={Vector} alt="vector" />
+              </div>
+
+              <div className="cardTitle">
+                <span>A single source of truth</span>
+              </div>
+            </div>
+
+            <p className="cardText">
+              When you add work to your Slate calendar we automatically
+              calculate useful insights
+            </p>
           </div>
-          <div className="frameContent">
-            <div className="contentRow">
-              <div className="rowTitle">
-                <div>
-                  <img src={Menu} alt="menu" />
-                </div>
-                <div className="title">
-                  <span>A single source of truth</span>
-                </div>
+
+          <div className="featureCard">
+            <div className="featureCardBody">
+              <div>
+                <img src={Interface} alt="vector" />
               </div>
-              <div className="rowText">
-                <p>
-                  When you add work to your Slate calendar we automatically
-                  calculate useful insights
-                </p>
+
+              <div className="cardTitle">
+                <span>Intuitive interface</span>
               </div>
             </div>
-            <div className="contentRow">
-              <div className="rowTitle">
-                <div>
-                  <img src={Vector} alt="vector" />
-                </div>
-                <div className="title">
-                  <span>Intuitive interface</span>
-                </div>
+
+            <p className="cardText">
+              When you add work to your Slate calendar we automatically
+              calculate useful insights
+            </p>
+          </div>
+
+          <div className="featureCard">
+            <div className="featureCardBody infinity">
+              <div>
+                <img src={Infinity} alt="infinity" />
               </div>
-              <div className="rowText">
-                <p>
-                  When you add work to your Slate calendar we automatically
-                  calculate useful insights
-                </p>
-              </div>
-            </div>
-            <div className="contentRow">
-              <div className="rowTitle infinity">
-                <div>
-                  <img src={Infinity} alt="infinity" />
-                </div>
-                <div className="title">
-                  <span>Or with rules</span>
-                </div>
-              </div>
-              <div className="rowText">
-                <p>
-                  When you add work to your Slate calendar we automatically
-                  calculate useful insights
-                </p>
+              <div className="cardTitle">
+                <span>Or with rules</span>
               </div>
             </div>
+            <p className="cardText">
+              When you add work to your Slate calendar we automatically
+              calculate useful insights
+            </p>
           </div>
         </div>
       </div>
